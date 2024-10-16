4d129589bdf7f2b4dc33393f9a056c315f863cd0
Nick Gaudio <gaudio.n@ufl.edu>
2024-10-15 20:02:28 -0400

    Added creating new user and joing group functions

--- Backend/api/src/main.py
+++ Backend/api/src/main.py
@@ -1,8 +1,9 @@
 # 172.20.1.197
 # uvicorn main:app --reload --host 172.20.1.197 --port 8000
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, HTTPException
 import motor.motor_asyncio
 from starlette.middleware.cors import CORSMiddleware
+from bson import ObjectId
 from typing import Annotated, Optional, List
 from pydantic import BaseModel, BeforeValidator, Field, ConfigDict
 
@@ -45,8 +46,47 @@ class UserCollection(BaseModel):
 @app.get("/users/", response_description="List of users", response_model=UserCollection,
     response_model_by_alias=False, )
 async def list_users():
+    #Get list of all user info
     users = await userCollection.find().to_list(1000)
     return UserCollection(users=users)
+
+@app.post("/user-add/", response_description="Add a new user", response_model=UserModel, 
+          response_model_by_alias=False)
+async def addUser(user: UserModel = Body(...)):
+    #Create new user
+    new_user = await userCollection.insert_one(user.model_dump(by_alias=True, exclude={"id"}))
+    created_user = await userCollection.find_one({"_id": new_user.inserted_id})
+    return created_user
+
+
+@app.put("/group-join/", response_description="Join a new group", response_model=UserModel)
+async def join_group(user_id: str, group_name: str):
+    # Find the user by id
+    if not ObjectId.is_valid(user_id):
+        raise HTTPException(status_code=400, detail="Invalid user_id format")
+
+    user_oid = ObjectId(user_id)  
+    
+    user = await userCollection.find_one({"_id": user_oid})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Check if the user is already part of the group
+    if group_name in user["groups"]:
+        raise HTTPException(status_code=400, detail="User already part of the group")
+
+    # Add the new group to the user's list of groups
+    await userCollection.update_one(
+        {"_id": user_oid},
+        {"$push": {"groups": group_name}}
+    )
+
+    # Fetch the updated user document
+    updated_user = await userCollection.find_one({"_id": user_oid})
+
+    return UserModel(**updated_user)
+
+
 
 
 @app.get("/")
