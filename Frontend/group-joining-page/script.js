7a641103c875ba20d9729470058b6afebd98e9ca
david-willis-dev <david.willis.dev@gmail.com>
2024-11-03 14:51:55 -0500

    Added basic group joining page

---
+++ Frontend/group-joining-page/script.js
@@ -0,0 +1,5 @@
+const groupName = 'My Group';
+const inviterName = 'John Doe';
+
+document.getElementById('group-name').textContent = groupName;
+document.getElementById('inviter-name').textContent = inviterName;
